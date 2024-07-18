d5d4a469ef771cd0d524a9401fdd259e876e490e
geekact <geekacting@gmail.com>
2024-07-18 21:45:02 +0800

    fix: cjs项目读取配置失败

--- test/lib/read-config.test.ts
+++ test/lib/read-config.test.ts
@@ -2,8 +2,7 @@ import { expect, test } from 'vitest';
 import { readConfig } from '../../src/lib/read-config';
 
 test('从根目录获取', async () => {
-  const config = await readConfig();
-  expect(config).toMatchInlineSnapshot(`
+  expect(readConfig()).toMatchInlineSnapshot(`
     [
       {
         "path": "./openapi/openapi.json",
