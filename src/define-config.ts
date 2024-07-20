65d100c0cfafce186bdabe6c24b1c2ddbbc32b7b
geekact <geekacting@gmail.com>
2024-07-20 15:10:04 +0800

    docs: 增加删除说明

--- src/define-config.ts
+++ src/define-config.ts
@@ -1,6 +1,6 @@
 export interface OpenapiClientConfig {
   /**
-   * openapi本地或者远程文件
+   * openapi本地或者远程文件，支持格式：`yaml | json`
    */
   path: string;
   /**
@@ -25,12 +25,14 @@ export interface OpenapiClientConfig {
   projectName?: string;
   /**
    * 类的生成方式。默认值：`method`
-   * - `method`，仅生成 `get|post|put|patch|delete` 几个方法，uri作为第一个参数传入
-   * - `uri`，把 method+uri 拼接成一个方法，比如 `POST /users/{id}` 会变成 `postUsersById()`
+   * - `method`，仅生成 **get|post|put|patch|delete** 几个方法，uri作为第一个参数传入
+   * - `uri`，把 method+uri 拼接成一个方法，比如 **POST /users/{id}** 会变成 **postUsersById()**
    */
   classMode?: 'method' | 'uri';
   /**
-   * 根据Tag生成不同的分组，以类似`client.user.getUsers()`这种方式调用。仅在 `classMode=uri` 场景下生效。默认值：`true`
+   * 根据Tag生成不同的分组，以类似 **client.user.getUsers()** 这种方式调用。仅在 `classMode=uri` 场景下生效。默认值：`true`
+   *
+   * 如果没有提供tags，则默认合并到`default`分组
    */
   tagToGroup?: boolean;
 }
